84d2f22ca9c82c95d1bdb43f7b3ee898caa9c0e8
bparmentier <bpadevel@gmail.com>
2013-04-25 12:50:20 +0200

    Move condition if result is drawn to SingleEliminationTournament

--- g38496/tournament/business/src/AbstractTournament.java
+++ g38496/tournament/business/src/AbstractTournament.java
@@ -72,15 +72,14 @@ abstract class AbstractTournament {
      * Sets the result of this match.
      * @param fId the id of the match
      * @param result the result of the match
-     * @throws TournamentException if match result is <code>DRAW</code> or
-     * <code>null</code>
+     * @throws TournamentException
      */
     public void setResult(int fId, ResultEnum result)
             throws TournamentException {
         Match match; // a match
-        
-        if ((result == ResultEnum.DRAW) || (result == null)) {
-            throw new TournamentException("Match result cannot be drawn");
+
+        if (result == null) {
+            throw new TournamentException("Match result cannot be null");
         } else {
             match = this.getMatch(fId);
             if (this.matchs.contains(match)) {
