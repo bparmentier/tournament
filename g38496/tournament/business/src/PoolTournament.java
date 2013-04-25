1094c034a441f5109be7f3bfd21f8f1dba2fee28
bparmentier <bpadevel@gmail.com>
2013-04-25 14:59:16 +0200

    Try to fix error when setting result in a pool

--- g38496/tournament/business/src/PoolTournament.java
+++ g38496/tournament/business/src/PoolTournament.java
@@ -134,9 +134,13 @@ public class PoolTournament {
      */
     public void setResult(int id, ResultEnum result) throws TournamentException {
         Pool pool;
-        for (int i = 0; i < this.pools.size(); i++) {
-            pool = this.pools.get(i);
-            pool.setResult(id, result);
-        }
+        //for (int i = 0; i < this.pools.size(); i++) {
+            for (int j = 0; j < this.getMatchs().size(); j++) {
+                if (this.getMatchs().get(j).getId() == id) {
+                    pool = this.pools.get(j);
+                    pool.setResult(id, result);
+                }
+            }
+        //}
     }
 }
