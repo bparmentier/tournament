baa6fb29a549ec45579584c2e64b1052fc361560
bparmentier <bpadevel@gmail.com>
2013-04-25 19:58:24 +0200

    Remove tournament v1 related files

--- g38496/tournament/business/src/v1/ResultEnum.java
+++
@@ -1,8 +0,0 @@
-package g38496.tournament.business;
-
-/**
- * Enumerates the different possible results of a match.
- *
- * @author g38496
- */
-public enum ResultEnum {NOT_PLAYED, PLAYER1, PLAYER2, DRAW}
